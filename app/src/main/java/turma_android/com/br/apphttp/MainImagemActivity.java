389f45b710d512fa52876a8aaa4cfef529431fbf
Florêncio Vieira Filho <florenciovieira@gmail.com>
2017-07-20 18:51:19 -0300

    Baixando Arquivos

---
+++ app/src/main/java/turma_android/com/br/apphttp/MainImagemActivity.java
@@ -0,0 +1,25 @@
+package turma_android.com.br.apphttp;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainImagemActivity extends AppCompatActivity {
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_imagem_layout);
+    }
+
+    public void baixarImagem(View v) {
+
+    }
+}
